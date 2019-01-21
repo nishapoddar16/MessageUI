f05d2fee803077273621c17cd8f59ec4767880ae
Tejaswi Tipparti <tejaswi@neetable.com>
2019-01-21 17:28:01 +0530

    Mail send added

--- MailCheck/ViewController.swift
+++ MailCheck/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    
+    @IBOutlet weak var textfield : UITextField!
+    @IBOutlet weak var subjectTextfield : UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
-
+    @IBAction func sendMail(_ sender : UIButton){
+        let mailComposeViewController = configureMailComposer()
+        if MFMailComposeViewController.canSendMail(){
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        }else{
+            print("Can't send email")
+        }
+    }
+    
+    func configureMailComposer() -> MFMailComposeViewController{
+        let mailComposeVC = MFMailComposeViewController()
+        mailComposeVC.mailComposeDelegate = self
+        mailComposeVC.setToRecipients([])
+        mailComposeVC.setSubject("subject for your e-mail")
+        mailComposeVC.setMessageBody("Write in your body for the e-mail", isHTML: false)
+        return mailComposeVC
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MailCheck' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MailCheck
+pod 'IQKeyboardManagerSwift'
+
+  target 'MailCheckTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MailCheckUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
