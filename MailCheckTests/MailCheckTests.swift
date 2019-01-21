96d3a93154e0d7fe8206e14ee6848f09d13cda83
Tejaswi Tipparti <tejaswi@neetable.com>
2019-01-21 14:39:05 +0530

    Initial Commit

---
+++ MailCheckTests/MailCheckTests.swift
@@ -0,0 +1,34 @@
+//
+//  MailCheckTests.swift
+//  MailCheckTests
+//
+//  Created by Tejaswi Tipparti on 21/01/19.
+//  Copyright © 2019 Tejaswi Tipparti. All rights reserved.
+//
+
+import XCTest
+@testable import MailCheck
+
+class MailCheckTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
